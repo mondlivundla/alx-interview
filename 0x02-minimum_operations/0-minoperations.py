5a81b73669486b2c36fec159a27a1dd4eadfc35a
mondlivundla <mondlivundla7@gmail.com>
2023-07-14 01:01:42 +0200

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    0-minOperations
+    Author - Mondliwethu Vundla
+"""
+def minOperations(n):
+    if (n < 2):
+        return 0
+    ops, root = 0, 2
+    while root <= n:
+        if n % root == 0:
+            ops += root
+            n = n / root
+            root -= 1
+        root += 1
+    return ops
